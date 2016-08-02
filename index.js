d54d4861cc3e8e78e11f5cc5e7639179828cf4ef
greatermeans <gakouri@gmail.com>
2016-08-01 21:22:41 -0400

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+function findTitles(event,birthday) {
+	event.preventDefault()
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "eebe610850914b5ab4d083370d48a2ee",
+        'begin_date': birthday.split('-').join(''),
+        'end_date': birthday.split('-').join('')
+    });
+    
+    debugger
+	// document.getElementById('query').reset()
+	$.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(result) {
+        displayHeadlines(result.response.docs)
+    }).fail(function(err) {
+        throw err;
+    });
+}
+
+function displayHeadlines(articles) {
+	const headlineList = `<ul>${articles.map(art => '<li>' +
+    `<a href="${art.web_url}" target="_blank"
+    > ${art.headline.main}</a>` + '</li>').join('')}</ul>`
+	$('#headlines')[0].innerHTML = headlineList
+}
+
+
+
